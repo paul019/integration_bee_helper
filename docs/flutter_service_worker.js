a667c44349d9b972f920d42229cfe45f3b6863ce
paul019 <39464035+paul019@users.noreply.github.com>
2024-04-13 14:08:45 +0200

    Create new export

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "bba7b81c8c83254be29e6d0530c43605",
-"index.html": "c7ffbf50322a241cbc3872c76b8a81a3",
+"index.html": "63981d8630de1ecba1177627a9dc59ba",
 "/": "38d1f4150e30dc63ccaebfcc9eaaa709",
-"main.dart.js": "c5ce92eb7b386ed5d784be33e87a0d81",
+"main.dart.js": "7950b2abb815418131a886dde1c36e54",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5215e91a06b99a35ab4966834919fb7c",
 "icons/Icon-192.png": "d1330b39856a27af480a363d67341327",
